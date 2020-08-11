05f84e810c92c931f413fa76b9f4b5e942b709b8
Jayraj Shah <jayrajcshah@gmail.com>
2020-08-11 16:47:51 +0530

    removed ads

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,15 @@
 const CACHE_NAME = "landing-cache";
 const urltocache = ["index.html", "offline.html"];
 
+// const self = this;
+// self.addEventListener("install", (event) => {
+//   console.log("install");
+// });
+
+// self.addEventListener("activate", (event) => {
+//   console.log("activate");
+// });
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -11,13 +20,15 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  caches.match(event.request).then(() => {
-    return fetch(event.request).catch(() => {
-      caches.match("index.html").catch(() => {
-        caches.match("offline.html");
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => {
+        caches.match("index.html").catch(() => {
+          caches.match("offline.html");
+        });
       });
-    });
-  });
+    })
+  );
 });
 self.addEventListener("activate", (event) => {
   const whiteList = [];
